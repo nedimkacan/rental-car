70a30ddb0b71699c5d782bc603d2455b0b7725f5
Nedim Kaçan <ndmkcn1453@gmail.com>
2023-08-18 00:03:25 +0300

    V1

---
+++ src/pages/user/reservations-page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserTemplate from '../../templates/user-template'
+
+const ReservationsPage = () => {
+  return (
+    <UserTemplate>ReservationsPage</UserTemplate>
+  )
+}
+
+export default ReservationsPage
